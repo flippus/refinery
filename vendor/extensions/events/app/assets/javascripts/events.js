b7f7f18d608a84cf18b82ab33bbbaafb6f17ef36
Philipp Neugebauer <flippus@nexgo.de>
2012-09-19 02:36:28 +0200

    javascripts works now complete

--- vendor/extensions/events/app/assets/javascripts/events.js
+++ vendor/extensions/events/app/assets/javascripts/events.js
@@ -2,12 +2,12 @@ function show_new_location()
 {
     document.getElementById('new_location').style.display='block';
     document.getElementById('location_select').style.display='none';
-    document.getElementById("location_selection").value = 1;
+    document.getElementById("location_selection").value = "1";
 }
 
 function show_location_select()
 {
     document.getElementById('location_select').style.display='block';
     document.getElementById('new_location').style.display='none';
-    document.getElementById("location_selection").value = 0;
+    document.getElementById("location_selection").value = "0";
 }
